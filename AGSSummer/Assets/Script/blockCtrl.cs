d86544871742f7e66c3e6116da9dd78fa6bab3c7
ombradiluna0507 <ombradiluna0507@gmail.com>
2020-04-15 08:25:28 +0900

    ブロック処理

---
+++ AGSSummer/Assets/Script/blockCtrl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockCtrl : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+
+    private Transform child;
+
+    private bool ctrlWaiver;
+
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        //生成時に落下しない処理
+        rigidbody2d.bodyType = RigidbodyType2D.Kinematic;
+
+        //回転する中心軸を取得
+        child = transform.Find("RotationCenter");
+
+        //ブロック操作が効く状態
+        ctrlWaiver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ctrlWaiver)
+        {
+            timeElapsed += Time.deltaTime;
+
+            //スペースまたは時間経過で落下開始
+            if (Input.GetKeyDown(KeyCode.Space) || timeElapsed >= 5)
+            {
+                //落下する
+                rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
+
+                //落下したら移動と回転ができなくなるよ
+                ctrlWaiver = true;
+
+                timeElapsed = 0;
+            }
+
+            //左右移動
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-0.2f, 0.0f, 0.0f);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(0.2f, 0.0f, 0.0f);
+            }
+
+            //子オブジェクト（中心軸）のRotationを回転させる
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+               child.Rotate(0.0f, 0.0f, 90.0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+               child.Rotate(0.0f, 0.0f, -90.0f);
+            }
+        }
+    }
+}
---
+++ AGSSummer/Assets/Script/createBlock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createBlock : MonoBehaviour
+{
+    public GameObject[] blockList;
+    private int blockNumber;
+
+    private float timeCreate;
+    private bool timerStart = false;
+
+    public float timeOut;
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //乱数取得
+        blockNumber = Random.Range(0, blockList.Length);
+
+        //ランダムにブロックの１つを生成
+        Instantiate(blockList[blockNumber], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!timerStart)
+        {
+            timeCreate += Time.deltaTime;
+
+            //スペースまたは時間経過でブロック生成を開始
+            if (Input.GetKeyDown(KeyCode.Space) || timeCreate >= 5)
+            {
+                timerStart = true;
+                timeCreate = 0;
+            }
+        }
+
+        if(timerStart)
+        {
+            //ブロック生成
+            Create();
+        }
+    }
+
+    void Create()
+    {
+        timeElapsed += Time.deltaTime;
+
+        //時間経過で生成
+        if (timeElapsed >= timeOut)
+        {
+            blockNumber = Random.Range(0, blockList.Length);
+            Instantiate(blockList[blockNumber], transform.position, Quaternion.identity);
+            timeElapsed = 0;
+            timerStart = false;
+        }
+    }
+}
