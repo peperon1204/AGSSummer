5508527fa853e6dbabefcd10cd8754cdfe900d3c
yuki1173 <yuki1173.reol@gmail.com>
2020-04-15 16:40:44 +0900

    大きいブロック

---
+++ AGSSummer/Assets/Script/bigBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bigBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.transform.localScale = new Vector2(2, 2);
+        }
+
+    }
+}
