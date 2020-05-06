7d4dc680d04f0d4937eff2fcd8a6555b27028ebe
peperon1204 <tmazxc1204@gmail.com>
2020-05-06 17:27:38 +0900

    Limitedモード追加

---
+++ AGSSummer/Assets/Script/LimitedMode/LimitedBlockCtrl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitedBlockCtrl : MonoBehaviour
+{
+    private Transform child;
+
+    private bool highSpeed;
+    private bool touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //回転する中心軸を取得
+        child = transform.Find("RotationCenter");
+
+        //true:高速落下     false:通常落下
+        highSpeed = false;
+
+        //true:触れている    false:触れていない
+        touch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!highSpeed)
+        {
+            //スペースで高速落下開始
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                //高速落下したら移動と回転ができなくなるよ
+                highSpeed = true;
+            }
+
+
+
+
+            //左右移動
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Translate(0.16f, 0.0f, 0.0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.Translate(-0.16f, 0.0f, 0.0f);
+            }
+
+            //子オブジェクト（中心軸）のRotationを回転させる
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                child.Rotate(0.0f, 0.0f, 90.0f);
+            }
+        }
+        else
+        {
+            if (!touch)
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    //高速落下していた場合高速落下解除
+                    highSpeed = false;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor" || other.gameObject.tag == "Block")
+        {
+            //床や他のブロックにぶつかったら移動と回転させない
+            highSpeed = true;
+            touch = true;
+        }
+    }
+}
---
+++ AGSSummer/Assets/Script/LimitedMode/LimitedCreateBlock.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitedCreateBlock : MonoBehaviour
+{
+   
+    public GameObject[] blockPattern1;      //この３つは
+    public GameObject[] blockPattern2;      //あとで直す
+    public GameObject[] blockPattern3;      //
+
+
+    private int patternNumber;
+    private int blockNumber;
+
+  //  private bool test; 
+
+    void Start()
+    {
+        patternNumber = Random.Range(0, 3);
+
+        blockNumber = 0;
+        //test = false;
+        Create();
+
+    }
+
+    void Update()
+    {
+      
+        if(blockNumber > 4)
+        {
+            blockNumber = 0;
+        }
+
+    }
+
+    public void Create()
+    {
+       switch(patternNumber)
+        {
+            case 0:
+                Instantiate(blockPattern1[blockNumber], new Vector3(0, 6 ,0), Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(blockPattern2[blockNumber], new Vector3(0, 6, 0), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(blockPattern3[blockNumber], new Vector3(0, 6, 0), Quaternion.identity);
+                break;
+            default:            
+                break;
+        }
+
+        //if (!test)
+        //{
+
+        //    test = true;
+        //    if (test)
+        //    {
+        //        //Create();
+        //        test = false;
+                blockNumber++;
+
+      //    }
+      //
+        //}
+    }
+
+    //void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (other.gameObject.tag == "Block")
+    //    {
+    //        stayTime += Time.deltaTime;
+    //    }
+    //}
+
+    //void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.gameObject.tag == "Block")
+    //    {
+    //        stayTime = 0;
+    //    }
+    //}
+}
+
