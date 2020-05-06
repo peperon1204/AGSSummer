f5665fdb774c0b76df18521b68fe423a30536665
yuki1173 <yuki1173.reol@gmail.com>
2020-05-06 18:30:27 +0900

    survivalモード更新

---
+++ AGSSummer/Assets/Script/survivalBlockCtrl/floorDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorDamage : MonoBehaviour
+{
+
+
+    public float powerBlock = 1; //攻撃力
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AGSSummer/Assets/Script/survivalBlockCtrl/survivalCtrl.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class survivalCtrl : MonoBehaviour
+{
+
+    public GameObject[] blockList;
+    private int blockNumber;
+
+    private float stayTime;
+
+    private bool start;
+
+    private float Timer;
+    private int seconds;
+    public Text timeText;
+    public Canvas canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //乱数取得
+        blockNumber = Random.Range(0, blockList.Length);
+
+        //Instantiate(blockList[blockNumber], transform.position, Quaternion.identity);
+
+        stayTime = 0;
+
+        start = true;
+
+        Timer = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        seconds = (int)Timer;
+
+        //timeText.text = seconds.ToString();
+
+        //ランダムにブロックの１つを生成
+        if (start && Timer <= 0)
+        {
+            Create();
+            start = false;
+            //canvas.enabled = false;
+        }
+
+        //段々と上に上がっていく
+        if (stayTime >= 5)
+        {
+            transform.Translate(0.0f, 1.0f, 0.0f);
+        }
+    }
+
+    public void Create()
+    {
+        blockNumber = Random.Range(0, blockList.Length);
+
+        Instantiate(blockList[blockNumber], transform.position, Quaternion.identity);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            stayTime += Time.deltaTime;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            stayTime = 0;
+        }
+    }
+
+}
