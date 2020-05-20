11d4a2f8a4316ff8fbdaa4deb7ecbb626298ace0
ombradiluna0507 <ombradiluna0507@gmail.com>
2020-05-20 12:30:06 +0900

    マルチシーン更新

---
+++ AGSSummer/Assets/Script/MultiPlay/MultiPlayMode/GetNumber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GetNumber : MonoBehaviour
+{
+    private int number;
+
+    private bool playGame;
+
+    public Text numberText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 1;
+        playGame = false;
+        DontDestroyOnLoad (this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playGame)
+        {
+            if(number < 4)
+            {
+                if(Input.GetMouseButtonDown(0))
+                {
+                    number += 1;
+                }
+            }
+
+            if(number > 1)
+            {
+                if(Input.GetMouseButtonDown(1))
+                {
+                    number -= 1;
+                }
+            }
+        }
+        numberText.text = number.ToString();
+    }
+}
--- AGSSummer/Assets/Script/MultiPlay/RaceScripts/RaceCtrl.cs
+++ AGSSummer/Assets/Script/MultiPlay/RaceScripts/RaceCtrl.cs
@@ -158,6 +158,5 @@ public class RaceCtrl : MonoBehaviour
                 }
             }
         }
-
     }
 }
