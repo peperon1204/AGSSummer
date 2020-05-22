c212487078362b5f33fcf756e4045731b79fa57a
peperon1204 <tmazxc1204@gmail.com>
2020-05-22 16:48:51 +0900

    アイテム更新

---
+++ AGSSummer/Assets/Script/SinglePlay/ItemBlockDestroy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBlockDestroy : MonoBehaviour
+{
+    public GameObject ItemDestroyBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左右移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            ItemDestroyBox.transform.Translate(1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ItemDestroyBox.transform.Translate(-1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            ItemDestroyBox.transform.Translate(0.0f, 0.1f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            ItemDestroyBox.transform.Translate(0.0f, -0.1f, 0.0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BlockWaiver")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
