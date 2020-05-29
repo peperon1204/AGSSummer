4dd0b351925d61c19adfc3ea23929bc34a9cbf7b
peperon1204 <tmazxc1204@gmail.com>
2020-05-29 14:40:22 +0900

    Sound追加

---
+++ AGSSummer/Assets/Script/ContinueBGM.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ContinueBGM : MonoBehaviour
+{
+    public bool destroyBGM;
+
+    // Start is called before the first frame update
+
+    // Use this for initialization
+    void Start()
+    {
+        destroyBGM = true;
+
+        if (destroyBGM)
+        {
+
+            // Sceneを遷移してもBGMが消えないようにする
+            DontDestroyOnLoad(this);
+
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MultiPlay" || SceneManager.GetActiveScene().name == "Menu")
+        {
+            
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log("name" + SceneManager.GetActiveScene().name);
+    }
+}
